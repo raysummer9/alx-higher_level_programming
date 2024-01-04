990b44a35e068f85b6c0da489946fbacc482fc30
raysummer9 <chimepromise433@gmail.com>
2024-01-04 10:23:37 +0100

    more strings manipulation

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = (str[39:65] + ' with python\n')
+str = (str[39:66] + ' with Python\n')
 print(str)
