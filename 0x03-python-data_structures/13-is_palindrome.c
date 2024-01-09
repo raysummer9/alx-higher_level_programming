b2e5111542051b6beb2cf3adb3d5d55d978f42ac
raysummer9 <chimepromise433@gmail.com>
2024-01-09 03:58:09 +0100

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,89 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverse(listint_t **head);
+int compare_lists(listint_t *head, listint_t * middle, int len);
+
+/**
+ * is_palindrome - checks if its palindrome
+ * @head: pointer head
+ * Return: 0
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int len, i;
+	listint_t *tmp;
+	listint_t *middle;
+
+	if (head == NULL || *head == NULL)
+		return (1);
+	tmp = *head;
+	middle = *head;
+
+	for (len = 0; tmp != NULL; len++)
+		tmp = tmp->next;
+
+	len = len / 2;
+	for (i = 1; i < len; i++)
+		middle = middle->next;
+	if (len % 2 != 0 && len != 1)
+	{
+		middle = middle->next;
+		len = len - 1;
+	}
+
+	reverse(&middle);
+	i = compare_lists(*head, middle, len);
+
+	return (i);
+}
+
+/**
+ * compare_lists - compare two lists
+ * @head: pointer to the head node
+ * @middle: pointer to the middle node
+ * @len: length list
+ * Return: 1 or 0
+ */
+
+int compare_lists(listint_t *head, listint_t *middle, int len)
+{
+	int i;
+
+	if (head == NULL || middle == NULL)
+		return (1);
+	for (i = 0; i < len; i++)
+	{
+		if (head->n != middle->n)
+			return (0);
+		head = head->next;
+		middle = middle->next;
+	}
+	return (1);
+}
+
+/**
+ * reverse -reverse a list
+ * @head: pointer to head
+ */
+
+void reverse(listint_t **head)
+{
+	listint_t *current;
+	listint_t *next;
+	listint_t *prev;
+
+	if (head == NULL || * head == NULL)
+		return;
+	prev = NULL;
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		current = next;
+	}
+	*head = prev;
+}
