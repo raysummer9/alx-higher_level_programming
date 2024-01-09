aaf32e9e7f5bde98177226907769169c1dc6875c
raysummer9 <chimepromise433@gmail.com>
2024-01-09 02:37:57 +0100

    task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sen_len = len(sentence)
+    if size > 0:
+        first_char = sentence[0]
+    else:
+        first_char = "None"
+    return (sen_len, first_char)
