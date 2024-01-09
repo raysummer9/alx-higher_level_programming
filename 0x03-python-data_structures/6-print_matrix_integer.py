31d7cbd6b251f6304812e15df0c7491e4975842c
raysummer9 <chimepromise433@gmail.com>
2024-01-09 02:17:03 +0100

    print matix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         for row in matrix:
             for num in row:
                 if num != row[-1]:
-                    print("{:d}".format(num), end =" ")
+                    print("{:d}".format(num), end=" ")
                 else:
                     print("{:d}".format(num))
     else:
