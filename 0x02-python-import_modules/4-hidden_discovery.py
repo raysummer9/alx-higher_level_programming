ec10fd72061fba3aaad430e941cc46bf3674d6ea
raysummer9 <chimepromise433@gmail.com>
2024-01-10 20:27:41 +0100

    print functions in a compiled file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from hidden_4 import *
     all_functions = dir()
-    for i in range(0, len(all_functions):
+    for i in range(0, len(all_functions)):
             if all_functions[i][:2] != "__":
                 print("{:s}".format(all_functions[i]))
         
