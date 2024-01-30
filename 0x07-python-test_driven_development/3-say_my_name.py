3bdf9f08d5688db83e763c535c8649a543a79476
Raysummer9 <chimepromise433@gmail.com>
2024-01-30 22:56:05 +0100

    Write a function that prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Function def for printing first name and last name"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print My name is <first name> <last name>.
+
+    Args:
+        first_name(string) the first value
+        last_name(string) the second value
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print ("My name is {:s} {:s}". format(first_name, last_name))
